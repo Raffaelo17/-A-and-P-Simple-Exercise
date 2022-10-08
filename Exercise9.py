7ef4d65b267cd2d6338ccf17660caed427429b8e
Raffaelo17 <christoffer.17.wijaya@gmail.com>
2022-10-08 23:25:52 +0700

    Done

---
+++ Exercise9.py
@@ -0,0 +1,9 @@
+def fun(str):
+
+    vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    if str == vowel:
+
+        print ("It is a vowel")
+    else:
+        print ("it is not vowel")
